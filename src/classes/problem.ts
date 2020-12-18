f6d4b764afbb742c01cf68342b254d25bdcb2e9d
Chaithanya Shyam D <codecraft15@gmail.com>
2020-12-19 00:28:44 +0530

    Add ProblemClass and ContestClass

--- src/classes/problem.ts
+++ src/classes/problem.ts
@@ -1,1 +1,21 @@
-export class ProblemClass {}
+export class ProblemClass {
+    index: string;
+    contestID: string;
+    problemID: string;
+    name: string;
+    tags: string[];
+    rating: string;
+    problemsetLink: string;
+    contestLink: string;
+
+    constructor (contestID: string, problemIndex: string, problemName = '', tags = [], rating = '') {
+        this.index = problemIndex;
+        this.contestID = contestID;
+        this.problemID = contestID + problemIndex;
+        this.name = problemName;
+        this.tags = tags;
+        this.rating = rating;
+        this.problemsetLink = `https://codeforces.com/problemset/problem/${contestID}/${problemIndex}`;
+        this.contestLink = `https://codeforces.com/contest/${contestID}/problem/${problemIndex}`;
+    }
+};
