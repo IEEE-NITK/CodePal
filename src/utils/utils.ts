4928d6dc934561e9e1a6e29f23e45764fbc24f28
lomadapraveenreddy <lomadapraveenreddy@gmail.com>
2020-12-18 18:54:52 +0530

    added workspace directory

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,7 +4,7 @@ import { ContestTreeItem } from "../data_providers/contests/contest_tree_item";
 import { ProblemTreeItem } from "../data_providers/problems/problem_tree_item";
 //import { RatingsTreeItem } from "../data_providers/ratings/ratings_tree_item";
 
-export const createContestDirectories = (contestID:string): void=>{
+export const createContestDirectories = (contestID:string,rootPath: string): void=>{
   //TODO: ADD FUNCTION THAT CREATES CONTEST DIRECTORY STRUCTURE.
 };
 
