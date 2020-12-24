df47976849160b0c50e712fbd7cf663dfd836ef4
lomadapraveenreddy <lomadapraveenreddy@gmail.com>
2020-12-24 14:38:45 +0530

    made changes

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,27 +2,32 @@ import * as vscode from "vscode";
 import { ContestClass } from "../classes/contest";
 import { ContestTreeItem } from "../data_providers/contests/contest_tree_item";
 import { ProblemTreeItem } from "../data_providers/problems/problem_tree_item";
-import {fetchContests as fetchContestsAPI} from "./api_calls";
+import { CodePalAPI } from "./api_calls";
 
-export const createContestDirectories = (contestID:string,rootPath: string): void=>{
-  //TODO: ADD FUNCTION THAT CREATES CONTEST DIRECTORY STRUCTURE.
-};
+export class Utils {
+  static createContestDirectories = (
+    contest: ContestClass | undefined,
+    rootPath: string
+  ): void => {
+    //TODO: ADD FUNCTION THAT CREATES CONTEST DIRECTORY STRUCTURE.
+    console.log(rootPath);
+  };
 
-export const getProblems = ():ProblemTreeItem[]=>{
-  return []; //TODO: CALL FUNCTION THAT FETCHES PROBLEMS IN PLACE OF [] 
-};
+  static getProblems = (): ProblemTreeItem[] => {
+    return []; //TODO: CALL FUNCTION THAT FETCHES PROBLEMS IN PLACE OF []
+  };
 
-// export const getRatings = ():RatingsTreeItem[]=>{
-//   return []; //TODO: CALL FUNCTION THAT FETCHES Ratings IN PLACE OF [] 
-// }
+  // static getRatings = ():RatingsTreeItem[]=>{
+  //   return []; //TODO: CALL FUNCTION THAT FETCHES Ratings IN PLACE OF []
+  // }
 
-export const fetchContests =async (type: string): Promise<ContestTreeItem[]> => {
-  let contests: ContestClass[]=await fetchContestsAPI(type); //TODO: CALL FUNCTION THAT FETCHES CONTESTS IN PLACE OF [] 
+  static fetchContests = async (type: string): Promise<ContestTreeItem[]> => {
+    let contests: ContestClass[] = await CodePalAPI.fetchContests(type); //TODO: CALL FUNCTION THAT FETCHES CONTESTS IN PLACE OF []
     return contests.map<ContestTreeItem>(
       (contest): ContestTreeItem => {
         return new ContestTreeItem(
           `${contest.contestID}`,
-          type === "Future"?"FutureContest":"contest",
+          type === "Future" ? "FutureContest" : "contest",
           type === "Future"
             ? vscode.TreeItemCollapsibleState.None
             : vscode.TreeItemCollapsibleState.Collapsed,
@@ -31,6 +36,5 @@ export const fetchContests =async (type: string): Promise<ContestTreeItem[]> => 
         );
       }
     );
- 
-  
-};
+  };
+}
