ef158e8294a0d66f5125efba58be9039e5332575
Janmansh <janmansh.191cs205@nitk.edu.in>
2020-12-25 20:47:26 +0530

    add sample test cases file in the project folder

--- src/features/folder_creation/test_cases_fetch.ts
+++ src/features/folder_creation/test_cases_fetch.ts
@@ -5,6 +5,39 @@ import * as vscode from "vscode";
 import { assert } from 'console';
 import { ProblemClass } from '../../classes/problem';
 
+
+const getInputOutput = async (problem1: ProblemClass) => {  
+    try {
+        const { data } = await axios.get(
+            `https://codeforces.com/contest/${problem1.contestID}/problem/${problem1.index}`
+        );
+        const $ = cheerio.load(data);
+        const postTitle = $('div > div.input > pre');
+        const postTitle1 = $('div > div.output > pre');
+        //console.log(postTitle.length);
+        let input:any = [];
+        let output:any = [];
+        const callbackFunctionInput = (i:any, element:any) => {
+            input.push($(element).text());
+        };
+
+        const callbackFunctionOutput = (i:any, element:any) => {
+            output.push($(element).text());
+        };
+
+        postTitle.each(callbackFunctionInput);
+        postTitle1.each(callbackFunctionOutput);
+
+        return {input,output};
+
+    }
+    catch (error) {
+        console.log("ERROR");
+        throw error;
+    }
+};
+
+
 export const fetchTestCases = async (
     problem: ProblemClass,
     folderPath: string
@@ -12,92 +45,31 @@ export const fetchTestCases = async (
     // TODO : Fill in the code for test case fetching 
     // Create a folder called 'tests' inside the folder 'problemFolderPath' 
     // and create input and output text files of the sample test cases
-    if(problem === undefined){
-        console.log('Empty Problem class');
-        return;
-      }
       
-      const getinput = async (problem1: ProblemClass) => {
-        try {
-          const { data } = await axios.get(
-            `https://codeforces.com/contest/${problem1.contestID}/problem/${problem1.index}`
-          );
-          const $ = cheerio.load(data);
-      
-              const postTitle = $('div > div.input > pre');
-              //console.log(postTitle.length);
-      
-              let input:any = [];
-      
-              const callbackFunction = (i:any, element:any) => {
-                  input.push($(element).text());
-              };
-      
-              postTitle.each(callbackFunction);
-      
-          return input;
-    
-        } catch (error) {
-              console.log("ERROR");
-          throw error;
-        }
-      };
+    const problemFolderPath = folderPath + `Tests/`;
+    //const problemFilePath = problemFolderPath + ``;
   
-      const getoutput = async (problem1: ProblemClass) => {
-        try {
-          const { data } = await axios.get(
-            `https://codeforces.com/contest/${problem1.contestID}/problem/${problem1.index}`
-          );
-          const $ = cheerio.load(data);
-      
-              const postTitle = $('div > div.output > pre');
-              //console.log(postTitle.length);
-      
-              let output:any = [];
-      
-              const callbackFunction = (i:any, element:any) => {
-                  output.push($(element).text());
-              };
-      
-              postTitle.each(callbackFunction);
-      
-          return output;
-        } catch (error) {
-              console.log("ERROR");
-          throw error;
-        }
-      };
-      
-      const problemFolderPath = folderPath + `Tests/`;
-      //const problemFilePath = problemFolderPath + ``;
-  
-      try{
+    try{
         await fs.mkdir(problemFolderPath);
-        getinput(problem)
-        .then((input) => {
-          for(let i=0; i<input.length; i++) {
-            const problemFilePath = problemFolderPath + `Input ${i}.txt`;
-            fs.writeFile(problemFilePath, input[i],);
-          }
+        getInputOutput(problem)
+        .then((data) => {
+            for(let i=0; i<data.input.length; i++) {
+                const problemFilePath = problemFolderPath + `Input ${i}.txt`;
+                fs.writeFile(problemFilePath, data.input[i],);
+            }
         });
   
-        getoutput(problem)
-        .then((output) => {
-          for(let i=0; i<output.length; i++) {
-            const problemFilePath = problemFolderPath + `Output ${i}.txt`;
-            fs.writeFile(problemFilePath, output[i],);
-          }
+        getInputOutput(problem)
+        .then((data) => {
+            for(let i=0; i<data.output.length; i++) {
+                const problemFilePath = problemFolderPath + `Output ${i}.txt`;
+                fs.writeFile(problemFilePath, data.output[i],);
+            }
         });
   
-      }
-      catch(err){
-        if(err.code === "EEXIST"){
-          console.log('Problem already exists');
-          vscode.window.showInformationMessage('Problem folder already exists');
-        }
-        else{
-          console.log('Unkown error');
-          vscode.window.showInformationMessage('Could not create folder');
-        }
-      }
+    }
+    catch(err){
+        console.log('Error');
+        vscode.window.showInformationMessage('Could not create test cases');
+    }
 };
