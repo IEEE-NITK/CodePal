7d0c0ebb9358554ef6b980f8e52413a1e625c9d5
Aditya <adityachirania@github.com>
2021-01-15 12:08:49 +0530

    fix browser issue

--- src/features/submit_problem/submit_problem.ts
+++ src/features/submit_problem/submit_problem.ts
@@ -1,4 +1,3 @@
-import * as open from "open";
 import * as vscode from "vscode";
 import { platform } from "os";
 import * as fs from "fs";
@@ -9,8 +8,8 @@ export const submitProblem = async (path: string) => {
     path = Utils.pathRefine(path, platform() === "linux" ? OS.linux : OS.windows);
     const jsonPath = path.substr(0, path.lastIndexOf("/")) + `/.problem.json`;
     const jsonData = JSON.parse(fs.readFileSync(jsonPath).toString());
-    open(`https://codeforces.com/contest/${jsonData["contestID"]}/submit/${jsonData["index"]}`);
-    vscode.window.showInformationMessage("submit problem page opened");
+    vscode.env.openExternal(vscode.Uri.parse(`https://codeforces.com/contest/${jsonData["contestID"]}/submit/${jsonData["index"]}`, true));
+    vscode.window.showInformationMessage("Submit problem page opened");
   } catch (err) {
     console.log(err);
     vscode.window.showErrorMessage(err);
